91d53353281cdec95f6de799b3ad3b7f180b0a09
prateek1o1 <prateekchhimwal@gmail.com>
2023-05-13 03:06:42 +0530

    late

--- MyTaskGuru/src/main/java/com/example/mytaskguru/MyTaskGuruApplication.java
+++ MyTaskGuru/src/main/java/com/example/mytaskguru/MyTaskGuruApplication.java
@@ -1,5 +1,8 @@
 package com.example.mytaskguru;
 
+import com.example.mytaskguru.services.UserService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,7 +14,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MyTaskGuruApplication {
-
+	private static final Logger logger= LogManager.getLogger(MyTaskGuruApplication.class);
 	@Configuration
 	@EnableWebMvc
 	public class webConfig implements WebMvcConfigurer {
@@ -26,6 +29,7 @@ public class MyTaskGuruApplication {
 	}
 
 	public static void main(String[] args) {
+		logger.info("This is an info log message");
 		SpringApplication.run(MyTaskGuruApplication.class, args);
 	}
 
